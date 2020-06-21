2a6790470bc4e9c1eb0fb7007f9a861685826322
unknown <lima.renan96@gmail.com>
2020-06-21 14:49:54 -0300

    update task

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import task_list, task_create, task_delete
+from .views import task_list, task_create, task_delete, task_update
 
 app_name = 'tasks'
 
@@ -7,5 +7,6 @@ urlpatterns = [
      path('list/', task_list, name='task_list'),
      path('create/', task_create, name='task_create'),
      path('delete/<int:pk_task>/', task_delete, name='task_delete'),
+     path('update/<int:pk_task>/', task_update, name='task_update'),
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 
+# list
 def task_list(request):
     tasks = Task.objects.all()
 
@@ -14,6 +15,7 @@ def task_list(request):
     return render(request, 'task_list.html', context)
 
 
+# create
 def task_create(request):
     if request.method == 'POST':
         title = request.POST['title']
@@ -27,8 +29,26 @@ def task_create(request):
     return render(request, 'task_create.html')
 
 
+# delete
 def task_delete(request, pk_task):
     task = get_object_or_404(Task, pk=pk_task)
     task.delete()
 
     return redirect('tasks:task_list')
+
+
+# update
+def task_update(request, pk_task):
+    task = Task.objects.get(pk=pk_task)
+    context = {
+        'task':task
+    }
+
+    if request.method == 'POST':
+        new_title = request.POST['title']
+        new_description = request.POST['description']
+        task.title = new_title
+        task.description = new_description
+        task.save()
+        return redirect('tasks:task_list')
+    return render(request, 'task_update.html', context)
