7a09a356cf914f066bdbdccaf8127c8c05447893
Renan Lima <renan@lima>
2020-06-24 09:48:25 -0300

    changes url

--- have2do/urls.py
+++ have2do/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/', include('tasks.urls', namespace='tasks')),
+    path('tasks/', include('tasks.urls', namespace='tasks')),
 ]
 
